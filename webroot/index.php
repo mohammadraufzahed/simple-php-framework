bc7b0b76316e85b103ed75e028d10c2f23649b42
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-07-03 15:05:58 +0430

    Update the index

--- webroot/index.php
+++ webroot/index.php
@@ -1,7 +1,8 @@
 <?php
 
+use JsPhpize\Nodes\Main;
+use SimplePHPFramework\controllers\MainController;
 use SimplePHPFramework\kernel\Router;
-use \SimplePHPFramework\controllers\ProductControllers;
 
 require __DIR__ . "/../vendor/autoload.php";
 
@@ -9,5 +10,6 @@ require __DIR__ . "/../vendor/autoload.php";
 $router = new Router();
 
 // You must declare the routers in this place
+$router->get('/', [new MainController, "index"]);
 
 $router->start();
