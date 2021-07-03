964a744b951a5a8b7f5b9ce84153021695e6f903
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-07-03 14:56:38 +0430

    Update the MainController

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -2,13 +2,22 @@
 
 namespace SimplePHPFramework\controllers;
 
+use SimplePHPFramework\kernel\View;
+
 require __DIR__ . "/../vendor/autoload.php";
 
 
 class MainController
 {
+    private View $viewEngine;
+    public function __construct()
+    {
+        // Initialize the viewEngine
+        $this->viewEngine = new View;
+    }
     public function index()
     {
-        echo "Index Hi";
+        // Render the template
+        return $this->viewEngine->render("index.pug", ["title" => "Simple PHP Framework"]);
     }
 }
