75f58df0489d9dc6e442a312100c534a7ac5649b
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-07-03 14:41:51 +0430

    Add the ViewEngine

---
+++ kernel/View.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SimplePHPFramework\kernel;
+
+use Pug\Pug;
+
+class View
+{
+    /**
+     * Render the page view
+     * @param string $template
+     */
+    static public function render(string $template, $params = [])
+    {
+        $viewDir = dir($_SERVER["DOCUMENT_ROOT"] . "/../views");
+        $templatePath = $viewDir->path . "/" . $template;
+        if (!file_exists($templatePath)) {
+            echo "Template not found";
+        } else {
+            $pugEngine = new Pug([
+                "pretty" => true,
+                "cache" => $_SERVER["DOCUMENT_ROOT"] . "/../views/cache"
+            ]);
+            $pugEngine->display($templatePath, $params);
+        }
+    }
+}
